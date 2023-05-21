50618a7a81a6ed87c7523e6577f09e48b24c44e8
fanny <fanny@gccompany.co.kr>
2023-05-22 07:26:21 +0900

    [Roman to Integer] Easy Level

---
+++ src/main/kotlin/RomanInteger.kt
@@ -0,0 +1,35 @@
+/**
+ * 13. Roman to Integer
+ *
+ * https://leetcode.com/problems/roman-to-integer/description/
+ * */
+
+fun main(args: Array<String>) {
+    RomanInteger().romanToInt("MDLX")
+}
+
+class RomanInteger {
+    fun romanToInt(s: String): Int {
+        val map = mutableMapOf(
+            'I' to 1,
+            'V' to 5,
+            'X' to 10,
+            'L' to 50,
+            'C' to 100,
+            'D' to 500,
+            'M' to 1000,
+        )
+
+        var number = 0
+
+        for (i in s.indices) {
+            val data = map.getValue(s[i])
+            if (i != s.length - 1 && data < map.getValue(s[i+1])) {
+                number -= data
+            } else {
+                number += data
+            }
+        }
+        return number
+    }
+}
